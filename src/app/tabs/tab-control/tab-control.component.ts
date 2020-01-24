1adf200f76183a243c50e26464ef241c9c7778de
Christian Liebel <christian.liebel@thinktecture.com>
2020-01-24 17:41:05 +0100

    feat(tabs): styling for TabControl

--- src/app/tabs/tab-control/tab-control.component.ts
+++ src/app/tabs/tab-control/tab-control.component.ts
@@ -4,6 +4,7 @@ import { TabPageComponent } from '../tab-page/tab-page.component';
 @Component({
   selector: 'app-tab-control',
   templateUrl: './tab-control.component.html',
+  styleUrls: ['./tab-control.component.css'],
 })
 export class TabControlComponent {
   @Input() selectedIndex = 0;
