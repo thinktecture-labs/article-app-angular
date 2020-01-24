110d96483ea1ccf54c9bab59dc12b4a4289e31bb
Christian Liebel <christian.liebel@thinktecture.com>
2020-01-24 17:56:13 +0100

    feat(article): drop-down boxes for article details

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { Article } from '../models/article';
+import { Category } from '../models/category';
 import { ArticleService } from '../services/article.service';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -11,19 +13,28 @@ import { ArticleService } from '../services/article.service';
 })
 export class ArticleDetailComponent implements OnInit {
   public article$: Observable<Article>;
+  public categories$: Observable<Category[]>;
+  public subcategories$: Observable<Category[]>;
+  public categoryChange = new ReplaySubject<string>(1);
 
-  // TODO: Lookup
-  // TODO: Dependent subcategories
+  // TODO: Rx-ify
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly articleService: ArticleService,
+    private readonly categoryService: CategoryService,
   ) {}
 
   public ngOnInit(): void {
+    this.categories$ = this.categoryService.getCategories();
+    this.subcategories$ = this.categoryChange.pipe(
+      switchMap(id => this.categoryService.getSubcategories(id)),
+    );
+
     this.article$ = this.activatedRoute.params.pipe(
       map(params => params.id),
-      switchMap(id => this.articleService.get(id))
+      switchMap(id => this.articleService.get(id)),
+      tap(article => this.categoryChange.next(article.category)),
     );
   }
 }
