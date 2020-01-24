070f2b681616823d82bfcb7443d90de86f88035f
Christian Liebel <christian.liebel@thinktecture.com>
2020-01-24 08:12:42 +0100

    feat(demo): add demo selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoSelectorComponent } from './demo-selector/demo-selector.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DemoSelectorComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoSelectorComponent } from './demo-selector/demo-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoSelectorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
